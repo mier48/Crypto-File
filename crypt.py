7d6bf7251184ddb591a6ccf692bfbcd6f0ecfd13
Alberto <info@albertomier.com>
2022-11-30 22:15:53 +0100

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -4,7 +4,7 @@ from termcolor import colored
 import os, sys
 # from os import listdir
 # from os.path import isfile, join
-# from os import walk
+from os import walk
 
 def encrypt(key, filename):
 	outFile = os.path.join(os.path.dirname(filename), "(encrypted)" + os.path.basename(filename))
@@ -37,6 +37,28 @@ def allfiles():
 			allFiles.append(os.path.join(dirpath, file))
 
 	return allFiles
+	
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
 display_info()
 choice = input("Do you want to (E)ncrypt or (D)ecrypt? ")
@@ -48,6 +70,10 @@ key_64 = base64.urlsafe_b64encode(key.encode("utf-8"))
 encFiles = allfiles()
 
 if choice == "E":
+	a = 0
+	print(f"Encriptando archivos")
+	l = len(encFiles)
+	printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 	for Tfiles in encFiles:	
 		if os.path.basename(Tfiles).startswith("(encrypted)"):
 			pass
@@ -55,13 +81,20 @@ if choice == "E":
 		elif Tfiles == os.path.join(os.getcwd(), sys.argv[0]):
 			pass 
 		else:
-			print(f"Encriptando archivo: {os.path.basename(Tfiles)}")
 			encrypt(key_64, str(Tfiles))
 			os.remove(Tfiles)
+		a = a+1
+		printProgressBar(a, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 
 elif choice == "D":
+	a = 0
+	print(f"Desencriptando archivos")
+	l = len(encFiles)
+	printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 	for Tfiles in encFiles:
 		if os.path.basename(Tfiles).startswith("(encrypted)"):
-			print(f"Desencriptando archivo: {os.path.basename(Tfiles)}")
 			decrypt(key_64, str(Tfiles))
 			os.remove(Tfiles)
+		a = a+1
+		printProgressBar(a, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
